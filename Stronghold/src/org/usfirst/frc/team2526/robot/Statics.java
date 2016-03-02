4ca17a3ace358f7dcf715ac4ebdec565054c8b17
alexkafer <alexkafer@gmail.com>
2016-03-01 19:16:32 -0600

    Updated dynamic statics. Any constant added here will be dynamically editable using RobotPreferences.

--- Stronghold/src/org/usfirst/frc/team2526/robot/Statics.java
+++ Stronghold/src/org/usfirst/frc/team2526/robot/Statics.java
@@ -1,61 +1,87 @@
 package org.usfirst.frc.team2526.robot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.wpi.first.wpilibj.Preferences;
 
 
 public class Statics {
 	Preferences prefs;
+	List<Constant> constants;
 	
-	// Defaults 
-	public static double LOADER_SPEED_IN = 0.5;
-	public static String LOADER_SPEED_IN_NAME = "loaderSpeedIn";
+	static Statics variables;
 	
-	public static double LOADER_SPEED_OUT = -0.5;
-	public static String LOADER_SPEED_OUT_NAME = "loaderSpeedPut";
-	
-	public static double LOADER_IN_DELAY = 1;
-	public static String LOADER_IN_DELAY_NAME = "loaderInDelay";
-	
-	public static double LOADER_OUT_DELAY = 1;
-	public static String LOADER_OUT_DELAY_NAME = "loaderOutDelay";
-	
-	
-	public static double CLIMBER_UP_SPEED = .5;
-	public static String CLIMBER_UP_SPEED_NAME = "climberUpSpeed";
-	
-	
-	public static double CLIMBER_EXTEND_LOADER_DELAY = 1;
-	public static String CLIMBER_EXTEND_LOADER_DELAY_NAME = "climberExtendLoaderDelay";
-	
-	public static double CATAPULT_HOLD_DELAY = .5;
-	public static String CATAPULT_HOLD_DELAY_NAME = "catapultHoldDelay";
-	
-	
-	public static double STRAIGHT_AUTO_DISTANCE_INCHES = 72;
-	public static String STRAIGHT_AUTO_DISTANCE_INCHES_NAME = "straightAutoDistanceInches";
-	
-	public static double DISTANCE_TO_SHOOT_INCHES = 144;
-	public static String DISTANCE_TO_SHOOT_INCHES_NAME = "distanceToShootInches";
-	
-	public static double TICKS_PER_INCH = 13.889;
-	public static String TICKS_PER_INCH_NAME = "ticksPerInch";
-	 
-	public void init() {
-		LOADER_SPEED_IN = prefs.getDouble("Loader_Speed_In", LOADER_SPEED_IN);
+	public Statics() {
+		prefs = Preferences.getInstance();
+		constants = new ArrayList<Constant>();
+		
+		// Add the constants here, name followed by its default
+		addConstant("Loader Speed In", 0.5);
+		addConstant("Loader Speed Out", -0.5);
+		addConstant("Loader In Delay", 1);
+		addConstant("Loader Out Delay", 1);
+		addConstant("Climber Up Speed", 0.5);
+		addConstant("Climber Extend Loader Delay", 1);
+		addConstant("Catapult Hold Delay", 0.5);
+		addConstant("Straight Auto Distance Inches", 72);
+		addConstant("Distance To Shoot Inches", 144);
+		addConstant("Ticks Per Inch", 13.889);
+		
+	}
+	/**
+	 * Just making sure we are all on the same page here
+	 * @return
+	 */
+	public static Statics getInstance() {
+		if (variables == null) {
+			variables = new Statics();
+		}
+		
+		return variables;
+	}
+	/**
+	 * Add a constant to the statics file. The cleanest way would to be adding the constant in the constructor
+	 * @param name to be used in RobotPreferences
+	 * @param value
+	 */
+	private void addConstant(String name, double value) {
+		constants.add(new Constant(name, value));
 	}
 	
-	public double getUpdatedPreference(String name, double def) {
-		return prefs.getDouble(name, def);
+	public static double getDouble(String string) {
+		Statics stat = getInstance();
+		
+		double defaultValue = 0;
+		
+		for (Constant constant : stat.constants) 
+			if (constant.key.equals(string)) 
+				defaultValue = constant.number;
+			
+		
+		return stat.prefs.getDouble(string, defaultValue);
+	}
+	
+	private class Constant
+	{
+	    String key;
+	    double number;
+
+	    public Constant(String key, double number)
+	    {
+	        this.key = key;
+	        this.number = number;
+	    }
 	}
 	
 	public static class Inches {
 		
 		public static double inchesToTicks(double inches) {
-			return Statics.TICKS_PER_INCH * inches;
+			return Statics.getDouble("Ticks Per Inch") * inches;
 		}
 		
 		public static double ticksToInches(double ticks) {
-			return ticks / Statics.TICKS_PER_INCH;
+			return ticks / Statics.getDouble("Ticks Per Inch");
 		}
 	}
 
